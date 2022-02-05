a72f9fd6ce6d44f59f458adb0d88a364c7162649
Tymoteusz <mekowskytymek@gmail.com>
2022-02-05 20:16:43 +0100

    delete

--- Covid19App/src/hooks/getCountryList.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default function getCountryList(setCountryNames) {
-  axios
-    .get("https://covid-api.mmediagroup.fr/v1/cases?country")
-    .then((response) => {
-      setCountryNames(Object.keys(response.data));
-    });
-}
