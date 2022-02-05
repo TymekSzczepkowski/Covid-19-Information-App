cb5483af2f596939ab051530bc8a7944d3863d38
Tymoteusz <mekowskytymek@gmail.com>
2022-02-05 19:53:24 +0100

    few changes

--- covidstats/src/App.js
+++ covidstats/src/App.js
@@ -2,7 +2,7 @@ import Main from "./components/Main/Main";
 import Layout from "./components/Layout";
 import useWebsiteTitle from "./hooks/useWebsiteTitle";
 function App() {
-  useWebsiteTitle("COVID-19 2022 | Check coronavirus status in your country");
+  useWebsiteTitle("COVID-19 | Check coronavirus status in your country");
   return (
     <Layout>
       <Main></Main>
